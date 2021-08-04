efe8dc5385b6770d4d92bd0f27f54dcac2390d36
Branux-sv <61380628+Branux-sv@users.noreply.github.com>
2021-08-04 00:17:51 -0600

    cleaning code

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,3 @@
-/*
-console.log('Client side JavaScript is loaded!')
-
-fetch('http://puzzle.mead.io/puzzle').then((resp) => {
-    resp.json().then( (data) => {        
-        console.log(data)
-    } )
-})
-*/
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
